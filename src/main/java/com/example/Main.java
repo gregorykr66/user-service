11ed7a592cd6750f9ab1281bda51e1a259f12059
GregoryKr <gregorykr66@gmail.com>
2020-11-30 13:24:27 +0100

    Add user-service

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -15,28 +15,16 @@ public class Main {
   }
 
   public static void main(String[] args) throws Exception {
-    /*
-    // Listener services use:
-    Service.service(new Subscription[]{
-        new Subscription("user-history", "buy-product", (body, sender) -> {
-          System.out.println("user-history: buy-product");
-        }),
-        new Subscription("user-history", "login", (body, sender) -> {
-          System.out.println("user-history: login");
-          sender.send("buy-product", "Coffee");
-        })
-    });
-    */
 
     // Gateway service uses:
-    Subscription.Sender globalSender = Service.service(new Subscription[]{
-        new Subscription("display", "display", (body, sender) -> {
-          System.out.println("display: display");
+    Service.service(new Subscription[]{
+        new Subscription("user", "fetch-product", (body, sender) -> {
+          System.out.println("user: fetch-product");
           System.out.println(body);
+          sender.send("fetch-stock", "Session_Id, Location, Product_id");
           // Push message over socket to the user
         })
     });
-    globalSender.send("display", "Everything works!");
 
     SpringApplication.run(Main.class, args);
   }
